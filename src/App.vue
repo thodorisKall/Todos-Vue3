6662397a5648c84df1da36db8890ef0df8965c9a
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2025-01-31 19:38:24 +0200

    docs: project initialized

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from "vue"
+
+const todos = ref([])
+const newTodo = ref("")
+</script>
+
+<template>
+  <header>
+    <h1>My Todos</h1>
+  </header>
+  <main>
+    <input placeholder="new todo" @keyup.enter="addTodo" v-model="newTodo" />
+    <button @click="addTodo">Add</button>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        {{ todo }}<button @click="deleteTodo(index)">Delete</button>
+      </li>
+    </ul>
+  </main>
+</template>
